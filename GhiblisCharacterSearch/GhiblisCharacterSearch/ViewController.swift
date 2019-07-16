80472f96a21c5ecf07ec4f3f55a7ff54488e34d9
prokhorova-anastasia <45105535+prokhorova-anastasia@users.noreply.github.com>
2019-07-16 22:49:07 +0400

    first commit

---
+++ GhiblisCharacterSearch/GhiblisCharacterSearch/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GhiblisCharacterSearch
+//
+//  Created by N on 16/07/2019.
+//  Copyright © 2019 N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ghibliImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
